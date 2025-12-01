56f9bc9837ccb0af0fe36bed767ffc2e808ad38a
DivineFavour <dominicdivinefavour9@gmail.com>
2025-12-01 10:09:00 +0100

    new com

--- script.js
+++
@@ -1,7 +0,0 @@
-let meanIcon =document.querySelector('#menu-icon');
-let navbar= document.querySelector('.navbar')
-
-meanIcon.onclick = () => {
-    meanIcon.classList.toggle('bx-x')
-    meanIcon.classList.toggle("active");
-}
